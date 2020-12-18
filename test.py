446f95269c8804603dd6d619a319417e52a1ecc1
ruixinn <75533464+ruixinn@users.noreply.github.com>
2020-12-18 11:58:32 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 data = "data1.txt" #input data file name
 
 with open(data) as file:
-    text = file.read().replace('\n', ' ')
+    text = file.read().replace('\n', ' ') #doesn't work lol
 
 import spacy
 nlp = spacy.load('en_core_web_sm') #lg
