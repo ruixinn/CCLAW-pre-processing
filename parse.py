bda4e0ce06dbc68437a0a177e7ee0c57c5156dc7
Chuen Kai <chuenkaiong@gmail.com>
2020-12-23 15:34:10 +0800

    added ability to parse multiple cases by paragraph number

--- parse.py
+++ parse.py
@@ -7,7 +7,9 @@ def parse_lines(lines):
     if line.split()[0] == str(counter):
       output.append(para)
       para = ""
-      counter += 1
-    para += line + " "    # The paragraphs are formatted such that spaces are omitted before the newline. We reinsert them here to avoid joining the last word of the current line with the first word of the next line
+      counter += 1    
+    para += line + " "          # The paragraphs are formatted such that spaces are omitted before the newline. We reinsert them here to avoid joining the last word of the current line with the first word of the next line
+    if line.split()[0] == "1":  # Reached end of case, start new case
+      counter = 2
   output.append(para)
   return output
